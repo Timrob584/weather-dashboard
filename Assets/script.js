dbf2702835098f8ceb9cec49d7b911f98a73200c
Tim Robinson <timrob584@gmail.com>
2021-09-14 16:47:11 -0400

    added 5 day forecast

--- Assets/script.js
+++ Assets/script.js
@@ -43,35 +43,68 @@ function getWeather(city) {
         getUvIndex(lat,lon, city)
     })
 
-    function getUvIndex(lat, lon, city) {
-        fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=&exclude=hourly,minutely,alerts&appid=" + key)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            // console.log(data);
-            uvIndex = data.current.uvi;
+function getUvIndex(lat, lon, city) {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=&exclude=hourly,minutely,alerts&appid=" + key)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        uvIndex = data.current.uvi;
 
-            var uvIndexEl = document.createElement("div");
-            uvIndexEl.innerHTML += "UV Index: ";
+        var uvIndexEl = document.createElement("div");
+        uvIndexEl.innerHTML += "UV Index: ";
 
-            var uvIndexBtn = document.createElement("button");
-            uvIndexBtn.innerHTML = uvIndex;
+        var uvIndexBtn = document.createElement("button");
+        uvIndexBtn.innerHTML = uvIndex;
             
-            uvIndexEl.appendChild(uvIndexBtn);
-            uv.appendChild(uvIndexEl);
+        uvIndexEl.appendChild(uvIndexBtn);
+        uv.appendChild(uvIndexEl);
     
 
-            if (uvIndex <=3) {
-                uvIndexBtn.setAttribute("class", "btn-success col-2")
-            } else if (uvIndex >3 && uvIndex <=8) {
-                uvIndexBtn.setAttribute("class", "btn-warning col-2")
-            } else (uvIndex >8) 
-                uvIndexBtn.setAttribute("class", "btn-danger col-2")
-            })
+        if (uvIndex <=3) {
+            uvIndexBtn.setAttribute("class", "btn-success col-1")
+        } else if (uvIndex >3 && uvIndex <=8) {
+            uvIndexBtn.setAttribute("class", "btn-warning col-1")
+        } else (uvIndex >8) 
+            uvIndexBtn.setAttribute("class", "btn-danger col-1")
+        getForecast(city)
+    })
+function getForecast(city) {
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&appid=" + key)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        var forecastEl = document.querySelectorAll(".forecast");
+        for (i=0; i<forecastEl.length; i++) {
+            forecastEl[i].innerHTML = "";
+            var forecastIndex = i*8 + 4;
+            var forecastDate = new Date(data.list[forecastIndex].dt * 1000);
+            var forecastDay = forecastDate.getDate();
+            var forecastMonth = forecastDate.getMonth() + 1;
+            var forecastYear = forecastDate.getFullYear();
+            var forecastDateEl = document.createElement("p");
+            forecastDateEl.setAttribute("class","mt-3 mb-0 forecast-date");
+            forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+            forecastEl[i].append(forecastDateEl);
+            var forecastWeatherEl = document.createElement("img");
+            forecastWeatherEl.setAttribute("class", "weather-icon");
+            forecastWeatherEl.src = "https://openweathermap.org/img/wn/" + data.list[forecastIndex].weather[0].icon + "@2x.png";
+            forecastEl[i].appendChild(forecastWeatherEl);
+            
+            var forecastTempEl = document.createElement("p");
+            forecastTempEl.innerHTML = "Temperature: " + Math.round(data.list[forecastIndex].main.temp) + " °F";
+            forecastEl[i].appendChild(forecastTempEl);
 
-      
+            var forecastHumidityEl = document.createElement("p");
+            forecastHumidityEl.innerHTML = "Humidity: " + data.list[forecastIndex].main.humidity + "%";
+            forecastEl[i].appendChild(forecastHumidityEl);
         }
+    })
+}
+       
+}
     };
 // To prevent default behavior on-click
 function formSubmit(e) {
@@ -80,6 +113,5 @@ function formSubmit(e) {
     getWeather(citySearch);
     searchInputEl.value="";
 }
-
 
 submitBtnEl.addEventListener("click", formSubmit);
