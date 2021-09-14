f782110f0d55fe05c8450fb1874c458f4343816c
Tim Robinson <timrob584@gmail.com>
2021-09-14 14:04:44 -0400

    added Temp

--- Assets/script.js
+++ Assets/script.js
@@ -5,17 +5,26 @@ var currentWeather = document.getElementById("current-title");
 var key ="b6202def895aaf2a9e59056af64e69fd";
 
 function getWeather(city) {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key)
-        .then(function(response) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + key)
+        .then(function (response) {
             return response.json();
         })
-        .then(function(data) {
-            // console.log(data);  
+        .then(function (data) {
+            console.log(data);
         var currentTitle = document.createElement("span");
         currentTitle.innerHTML = city + " ";
         currentTitle.innerHTML +=moment().format("MM/DD/YYYY");
         currentWeather.appendChild(currentTitle);
         })
+
+        var weatherIcon = document.createElement("img");
+        weatherIcon.setAttribute("class", "weather-icon");
+
+        // weatherIcon.src ="https://openweathermap.org/img/wn/" + data.weather["0"].icon + "@2x.png";
+        // currentTitle.appendChild(weatherIcon);
+
+        var tempEl = document.createElement("p");
+        tempEl.innerHTML = "Temp:" + data.main.temp + " °F";
 }
 // To prevent default behavior on-click
 function formSubmit(e) {
